b52671d2fc3a59012de5195076f89a5a0acefb28
KwonGusung <create2879@gmail.com>
2019-07-28 16:25:41 +0900

    url validation 추가

--- src/main/java/com/peachybloom/controller/ShorteningUrlController.java
+++ src/main/java/com/peachybloom/controller/ShorteningUrlController.java
@@ -4,6 +4,7 @@ import com.peachybloom.ApiResponse;
 import com.peachybloom.ApiResponse.ApiResponseBuilder;
 import com.peachybloom.domain.model.UrlConvertModel;
 import com.peachybloom.service.ShorteningUrlService;
+import org.hibernate.validator.constraints.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
 
 @Controller
+@Validated
 public class ShorteningUrlController {
 
     private static final Logger logger = LoggerFactory.getLogger(ShorteningUrlController.class);
@@ -28,7 +36,7 @@ public class ShorteningUrlController {
 
     @ResponseBody
     @GetMapping("/convert")
-    public ResponseEntity<ApiResponse<UrlConvertModel>> convert(@RequestParam String originUrl) {
+    public ResponseEntity<ApiResponse<UrlConvertModel>> convert(@RequestParam @Valid @URL String originUrl) {
         return ResponseEntity.ok(ApiResponseBuilder.builder()
                 .data(shorteningUrlService.convert(originUrl))
                 .status(HttpStatus.OK)
@@ -36,7 +44,7 @@ public class ShorteningUrlController {
     }
 
     @GetMapping("/redirect")
-    public String redirect(@RequestParam String location) {
+    public String redirect(@RequestParam @Valid @URL String location) {
         return "redirect:" + location;
     }
 
